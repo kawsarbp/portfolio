cbd34a6b42fa799e765c5fcdbfbd9f7275c7490d
Kawsar Ahmed Ashif <96182258+kawsarbp@users.noreply.github.com>
2024-03-11 13:23:26 +0600

    configuration done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}","./**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}","./**/*.{html,js}","./node_modules/flowbite/**/*.js"],
     theme: {
         extend: {
             container: {
@@ -15,5 +15,7 @@ module.exports = {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
